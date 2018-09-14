6fe2a9386b8c74457ecabe5a3be8c8e6606a6157
ciatph <ciat.ph@gmail.com>
2018-09-14 17:34:42 +0700

    Added main app layout.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Quickstart</title>
+</head>
+<body>
+
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <!-- Navbar contents -->
+        </nav>
+    </div>
+
+    @yield('content')
+
+</body>
+</html>
