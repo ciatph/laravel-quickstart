85f87c6f4f7ffea874726401d2ed7ae305ebf2fa
ciatph <ciat.ph@gmail.com>
2018-09-14 16:57:22 +0700

    Updated typo error.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::dafaultStringLength(191);
+        Schema::defaultStringLength(191);
     }
 
     /**
